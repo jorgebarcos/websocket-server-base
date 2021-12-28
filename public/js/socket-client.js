eaafc0ab2d175a033b9be2d87cd31f3e2f0c75d7
Jorge Barcos <jorgebarcos88@gmail.com>
2021-12-28 12:13:34 -0300

    Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,19 @@
+// Referencias del html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('Conectado al servidor');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+})
+
+
+socket.on('disconnect', () => {
+    console.log('Desconectado del servidor');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+})
