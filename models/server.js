3120b65a02f1dff3aaf6f5f29e82ea990e85a768
Jorge Barcos <jorgebarcos88@gmail.com>
2021-12-28 10:53:35 -0300

    Inicio del proyecto - Fundamentos sobre Web Sockets

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors')
+
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT
+        this.paths = {}
+
+
+        // Global Middlewares
+        this.middlewares();
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+
+    middlewares() {
+        // CORS
+        this.app.use(cors());
+        // Public directory
+        this.app.use(express.static('public'));
+
+    }
+
+
+    routes() {
+        // this.app.use(this.paths.auth, require('../routes/auth'));
+    }
+
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port)
+        })
+    }
+
+}
+
+module.exports = Server;
